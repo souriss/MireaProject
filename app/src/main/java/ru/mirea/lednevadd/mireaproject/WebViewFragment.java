40360f0643df64df4f1a0a1fe2bfffab495c5d3c
Диана Леднева <132996319+souriss@users.noreply.github.com>
2024-04-04 20:50:30 +0300

    Initial commit

---
+++ app/src/main/java/ru/mirea/lednevadd/mireaproject/WebViewFragment.java
@@ -0,0 +1,29 @@
+package ru.mirea.lednevadd.mireaproject;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class WebViewFragment extends Fragment {
+
+    private WebView webView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_web_view, container, false);
+
+        webView = view.findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webView.loadUrl("https://www.marvel.com/");
+
+        return view;
+    }
+}
